32bae78671981fd31d9ce44bd587b46c7b683e15
Aquilusdev <Aquilussolutions@gmail.com>
2024-11-20 23:02:46 +0530

    Add files via upload

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
 function Hero() {
-    return (
-        <div>
-
-            <h1>Hero</h1>
-
-        </div>
-    );
+  return (
+    <section className="container">
+      <div className="font-bold text-center md:text-left text-2xl">Lorem ipsum dolor sit amet.</div>
+      <div className="text-lg">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis cumque
+        eligendi nesciunt fugit optio veritatis dicta voluptas ad alias
+        reiciendis, qui, deserunt vitae error laudantium sapiente nulla
+        perferendis neque a animi quisquam, rerum laborum!
+      </div>
+      <button className="bg-blue-600 text-center font-medium">
+        Schedule A Call
+      </button>
+    </section>
+  );
 }
 
 export default Hero;
