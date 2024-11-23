28d979c9c68b6f823876fdfd6b191f1ee25a5ae0
Aquilusdev <Aquilussolutions@gmail.com>
2024-11-23 13:45:22 +0530

    Add files via upload

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,23 +1,23 @@
 const ServiceCard = ({ service, desc, color, icon }) => {
   return (
     <div
-      className="card flex flex-col items-center justify-center p-6 transition-transform transform hover:scale-105"
+      className="card flex flex-col items-center justify-center m-6 transition-transform transform hover:scale-105 rotate-2 hover:rotate-0   "
       key={color}
     >
       <div
-        className={`container-card bg-gradient-to-br from-blue-100 via-blue-200 to-blue-300 shadow-lg rounded-2xl p-6 relative`}
+        className={`container-card bg-gradient-to-br from-red-100 via-red-200 to-red-500 hover:bg-red-500 hover:bg-gradient-to-br hover:from-red-300 hover:to-red-500 transition  shadow-lg rounded-2xl p-6 relative`}
       >
         {/* Icon Section */}
         <div className="icon-container flex justify-center mb-4">
           <img
-            src={icon} // Service-specific icon
+            src={icon} 
             alt={`${service} icon`}
-            className="w-16 h-16 rounded-full"
+            className="w-50 h-50"
           />
         </div>
         {/* Title and Description */}
-        <p className="card-title text-xl font-bold text-center mt-2 text-blue-800">
-          {service}
+        <p className="card-title text-xl font-bold text-center mt-2 text-red-800">
+          {service.toUpperCase()}
         </p>
         <p className="card-description text-base text-center text-gray-700 mt-4 leading-relaxed">
           {desc}
@@ -26,7 +26,7 @@ const ServiceCard = ({ service, desc, color, icon }) => {
         <div className="text-center mt-10">
           <a
             href="#"
-            className="bg-gradient-to-r from-blue-500 to-blue-600 text-white font-medium px-6 py-3 rounded-lg shadow-md hover:shadow-lg transition-all hover:from-blue-600 hover:to-blue-700 hover:-translate-y-1"
+            className="bg-gradient-to-r from-red-500 to-red-600 text-white font-medium px-6 py-3 rounded-lg shadow-md hover:shadow-lg transition-all hover:from-red-600 hover:to-red-700 hover:-translate-y-1"
           >
             Book A Call Now
           </a>
